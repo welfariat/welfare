3517dfb9ffd2b5038b375323f86d1ef8d0b87147
welfariat <welfare@protonmail.com>
2020-03-19 17:09:39 +0800

    rename them

--- greasy-scripts/jkforum-image-load.js
+++ greasy-scripts/jkforum-image-load.js
@@ -3,26 +3,25 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.jkforum.net/thread-*
 // @grant       none
-// @version     1.01
+// @version     1.1.0
 // @author      zbutfly@gmail.com
 // @icon        data:image/webp;base64,UklGRiwCAABXRUJQVlA4ICACAACQDwCdASpgAGAAPpFCmUklpCIhKrQLULASCWkACwLnM7lZUD3qPhhyfoi4EJqYEkijj1eyOYmT3IYhtxgy+BGpwZDnJebEYh8xoDtcQGJwfXR6XKUsDp8ImRstvpI9QDDlilTxpA7kcdGt/DL01It0ORjvjrZdFshyhgDOqb2u0sSMxP4AAP75WILI9izCJ90U/ymzkAkChdxajHRzZduKaHsdj1+2UJNefVhNU13bUAPiqJsdwBBpOrKaP5IEU7r53aXsSsXcLRe+kMYBXu1yVLxOBbqz5FzIX0RejZ9rMQ4JezrYUOPmp2+nW63W5ZTyR4L1Kb6dYBQad9bw7txNBTLTh+0tHFjHb+iUGu44OJbc59XGEu0e95Du9epPGLsCjkKOTjpoQEVTQOG6VQX9JFLIBf1tS3TX1dcrcoE4AwJuz/DQqdfVAzkCKGorgYfGEEh07+04xxg84GWco2z0EZapsSzjJ+I5rtWG0v0YP+O4n6QalKIK3Yen+6+lF4np3zOYHprBKtih8L23RFapNjSV1cX/BiR3S52ev9J5OndAgVhpeex6DCEUkRV9l42knag8br10z3f2g/3iZdXGBgYGcnWtARJDCbnUHGcMStX/diAZtkphXJz19NMWf1k3YlsHc0IDJOgUe1sAntBhE+w9NTprFM4ezX2WFD7cosQxHWTRYCKQ2hC4uYI7CbgoCd8uBMxuNS+pMR45QAAA
 // @description 捷克论坛图片主题自动感谢和载入全图
 // ==/UserScript==
 
 ; (function () { // eslint-disable-line
+	var imgs = document.getElementsByTagName("img");
+	for (var i = 0; i < imgs.length; i++) {
+		var zoom = imgs[i].getAttribute("zoomfile");
+		if (zoom) {
+			console.log("载入原图：" + zoom + "\n缩略图（" + imgs[i].naturalWidth + " x " + imgs[i].naturalHeight + "）：" + imgs[i].src)
+			imgs[i].src = zoom;
+			imgs[i].onload = e => e.target.title = "原图已载入：" + e.target.naturalWidth + " x " + e.target.naturalHeight;
+		}
+	}
 	var thanks = document.getElementById('k_thankauthor');
 	if (thanks) {
 		alert("自动感谢，等待刷新后载入全图。");
 		thanks.click();
-	} else {
-		var imgs = document.getElementsByTagName("img");
-		for (var i = 0; i < imgs.length; i++) {
-			var zoom = imgs[i].getAttribute("zoomfile");
-			if (zoom) {
-				console.log("载入原图：" + zoom + "\n缩略图（" + imgs[i].naturalWidth + " x " + imgs[i].naturalHeight + "）：" + imgs[i].src)
-				imgs[i].src = zoom;
-				imgs[i].onload = e => e.target.title = "原图已载入：" + e.target.naturalWidth + " x " + e.target.naturalHeight;
-			}
-		}
 	}
 }());
--- greasy-scripts/pinterest-original-image.js
+++ greasy-scripts/pinterest-original-image.js
@@ -1,9 +1,10 @@
+
 // ==UserScript==
 // @name        pinterest-original-image
 // @namespace   Violentmonkey Scripts
 // @match		*://*.pinterest.com/pin/*
 // @grant       none
-// @version     1.00
+// @version     1.1.0
 // @author      zbutfly@gmail.com
 // @description Pinterest Original Image Dimensions and Load
 // @icon		data:image/webp;base64,UklGRiwCAABXRUJQVlA4ICACAACQDwCdASpgAGAAPpFCmUklpCIhKrQLULASCWkACwLnM7lZUD3qPhhyfoi4EJqYEkijj1eyOYmT3IYhtxgy+BGpwZDnJebEYh8xoDtcQGJwfXR6XKUsDp8ImRstvpI9QDDlilTxpA7kcdGt/DL01It0ORjvjrZdFshyhgDOqb2u0sSMxP4AAP75WILI9izCJ90U/ymzkAkChdxajHRzZduKaHsdj1+2UJNefVhNU13bUAPiqJsdwBBpOrKaP5IEU7r53aXsSsXcLRe+kMYBXu1yVLxOBbqz5FzIX0RejZ9rMQ4JezrYUOPmp2+nW63W5ZTyR4L1Kb6dYBQad9bw7txNBTLTh+0tHFjHb+iUGu44OJbc59XGEu0e95Du9epPGLsCjkKOTjpoQEVTQOG6VQX9JFLIBf1tS3TX1dcrcoE4AwJuz/DQqdfVAzkCKGorgYfGEEh07+04xxg84GWco2z0EZapsSzjJ+I5rtWG0v0YP+O4n6QalKIK3Yen+6+lF4np3zOYHprBKtih8L23RFapNjSV1cX/BiR3S52ev9J5OndAgVhpeex6DCEUkRV9l42knag8br10z3f2g/3iZdXGBgYGcnWtARJDCbnUHGcMStX/diAZtkphXJz19NMWf1k3YlsHc0IDJOgUe1sAntBhE+w9NTprFM4ezX2WFD7cosQxHWTRYCKQ2hC4uYI7CbgoCd8uBMxuNS+pMR45QAAA
